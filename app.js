5d4e245652337252ff6a49a81d7fe03cf8b8a0a2
Jackson Currie <30005345@g.toiohomai.ac.nz>
2018-08-01 17:25:24 +1200

    add files

--- app.js
+++ app.js
@@ -6,14 +6,57 @@ const forw = new Gpio(15, {mode: Gpio.OUTPUT});
 const up = new Gpio(27, {mode: Gpio.OUTPUT});
 const ins = new Gpio(24, {mode: Gpio.OUTPUT});
 
-// Start with roboarm in centre
-var rotValue = 1450;
-var forwValue = 1250;
-var upValue = 1500;
-var inValue = 1700;
+var moveServo = (servo, position, final, callback) => {
+   
+    // While not complete
+    if(position != final) {
+       
+        // Which way to move
+        position += (position < final) ? 5 : -5;
 
-// Move servos to centre
-rot.servoWrite(rotValue);
-forw.servoWrite(forwValue);
-up.servoWrite(upValue);
-ins.servoWrite(inValue);
+        // Send to servo
+        servo.servoWrite(position)
+    
+        // Run function again
+        setTimeout(() => 
+            moveServo(servo, position, final, callback),
+            10
+        );
+    } else {
+        callback();
+    }
+}
+
+// Start automation of arm
+var automate = () => {
+
+    // Moves the servos
+    moveServo(rot, 1450, 2360, () =>
+    moveServo(up, 1500, 1300, () =>
+    moveServo(forw, 1250, 1700, () =>
+    moveServo(ins, 1700, 1100, () =>
+    moveServo(up, 1300, 1500, () =>
+    moveServo(forward, 1700, 1250, () =>
+    moveServo(rotate, 2360, 540, () =>
+    moveServo(up, 1500, 1300, () =>
+    moveServo(forw, 1250, 1700, () =>
+    moveServo(ins, 1100, 1700, () =>
+    moveServo(forw, 1700, 1250, () =>
+    moveServo(up, 1300, 1500, () =>
+    moveServo(rot, 540, 1450, () =>
+    moveServo(rot, 1450, 540, () =>
+    moveServo(up, 1500, 1300, () =>
+    moveServo(forw, 1250, 1700, () =>
+    moveServo(ins, 1700, 1100, () =>
+    moveServo(up, 1300, 1500, () =>
+    moveServo(forw, 1700, 1250, () =>
+    moveServo(rot, 540, 2360, () =>
+    moveServo(up, 1500, 1300, () =>
+    moveServo(forw, 1250, 1700, () =>
+    moveServo(ins, 1100, 1700, () =>
+    moveServo(forw, 1700, 1250, () =>
+    moveServo(up, 1300, 1500, () =>
+    moveServo(rot, 2360, 1450, () =>
+    automate()
+    ))))))))))))))))))))))))));
+}
